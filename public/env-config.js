1a00d70a73865fc75b6799c3bbb1a724d6a06c6a
XCR-hub <tcerda@xcr.fr>
2025-10-07 10:37:16 +0200

    Updated env-config.js

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,5 @@
 // Configuration Supabase pour la production
 window.ENV = {
   VITE_SUPABASE_URL: 'https://viuuznfqkauatkjcegcj.supabase.co',
-  VITE_SUPABASE_ANON_KEY: 'VOTRE_VRAIE_CLE_ICI'
+  VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpdXV6bmZxa2F1YXRramNlZ2NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzNzMzNDAsImV4cCI6MjA0ODk0OTM0MH0.D0wo88ypG2OiZL3wCiUGgMyA3OaqzIjKU2Nbo-oxOjA'
 };
